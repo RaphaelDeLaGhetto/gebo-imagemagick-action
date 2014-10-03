17ee2a7588c49c9de93a6dfd48c98ff0c671aa76
RaphaelDeLaGhetto <daniel@capitolhill.ca>
2014-10-03 15:39:34 -0600

    Get incipient gebo style action

--- actions/index.js
+++ actions/index.js
@@ -1,142 +1,80 @@
-var archiver = require('archiver'),
-    exec = require('child_process').exec,
-    fs = require('fs'),
-    mime = require('mime'),
-    mkdirp = require('mkdirp'),
+'use strict';
+
+var fs = require('fs'),
+    lib = require('../lib'),
     nconf = require('nconf'),
     q = require('q'),
     winston = require('winston');
 
-// Logging stuff           
-nconf.file({ file: './gebo.json' });
-var logLevel = nconf.get('logLevel'),
-    timeout = nconf.get('imagemagick').timeout;
-var logger = new (winston.Logger)({ transports: [ new (winston.transports.Console)({ colorize: true }) ] });
+module.exports = function() {
 
+    nconf.file({ file: './gebo.json' });
+    // Logging stuff
+    var logLevel = nconf.get('logLevel');
+    var logger = new (winston.Logger)({ transports: [ new (winston.transports.Console)({ colorize: true }) ] });
 
-/**
- * Convert a given file from one format to another
- * 
- * @param string
- * @param string
- * @param string
- *
- * @return string
- */
-function _convert(path, format, outdir) {
-    var deferred = q.defer();
+    // For producing the download URL
+    var domain = nconf.get('domain');
 
-    if (!outdir) {
-      outdir = '.';
-    }
+    /**
+     * Convert an image or PDF from one format to another
+     * 
+     * @param Object
+     * @param Object
+     * 
+     * @return promise
+     */
+    exports.convert = function(verified, message) {
+        var deferred = q.defer();
 
-    var outputFileName = _getOutputFileName(path, format);
+        if (verified.admin || verified.execute) {
 
-    // Set up time limit
-    var executionTimer = setTimeout(function() {
-            var kill = 'kill $(cat /tmp/' + _getOutputFileName(path, format) + '.pid)';
-            exec(kill, function(err, stdout, stderr) {
-                deferred.resolve({ error: 'Sorry, that file took too long to process' }); 
+          var destDir = './public/' + message.file.path.split('/').pop();
+          lib.convert(message.file.path, message.content.format, destDir).
+            then(function(path) {
+
+                // Respect the original file name
+                var filename = lib.getOutputFileName(message.file.originalname, message.content.format);
+ 
+                var newPath = path.split('/');
+                newPath.pop();
+                newPath.push(filename);
+                newPath = newPath.join('/');
+
+                fs.rename(path, newPath, function(err) {
+                    if (err) {
+                      if (logLevel === 'trace') logger.error('fs.rename:', err);
+                      deferred.resolve({ error: err });
+                    }
+
+                    if (message.content.raw) {
+                      deferred.resolve({ filePath: newPath, fileName: filename });
+                    }
+                    else {
+                      deferred.resolve(domain + '/' + message.file.path.split('/').pop() + '/' + filename);
+                    }
+                  });
+//                fs.writeFile(destDir + filename, text, function(err) { 
+//                    if (message.content.raw) {      
+//                      fs.realpath(destDir + filename, function(err, resolvedPath) {
+//                            deferred.resolve({ filePath: resolvedPath, fileName: filename });
+//                        });
+//                    }
+//                    else {
+//                      deferred.resolve(domain + '/' + message.file.path.split('/').pop() + '/' + filename);
+//                    }
+//                  });
+              }).
+            catch(function(err) {
+                deferred.resolve({ error: err });
               });
-        }, timeout);
-
-    mkdirp(outdir, function(err, made) {
-        if (err) {
-          if (logLevel === 'trace') logger.error('gebo-imagemagick, mkdirp:', err);
-          deferred.resolve({ error: err });
+        }
+        else {
+          deferred.reject('You are not permitted to request or propose that action');
         }
 
-        // PDFs may produce multiple images
-        var command = 'convert ' + path + ' ' + outdir + '/';
-        if (mime.lookup(path) === 'application/pdf') {
-          command += '%0d_';
-        }
-        command += outputFileName + ' & echo $! > /tmp/' + outputFileName + '.pid';
+        return deferred.promise;
+      };
 
-
-        if (logLevel === 'trace') logger.info('gebo-imagemagick:', command);
-    
-        exec(command, function(err, stdout, stderr) {
-            clearInterval(executionTimer);
-            if (err) {
-              if (logLevel === 'trace') logger.error('gebo-imagemagick:', err);
-              deferred.resolve({ error: err });
-            }
-            else {
-              if (logLevel === 'trace' && stderr) logger.warn('gebo-imagemagick:', stderr);
-              fs.realpath(outdir, function(err, resolvedPath) {
-                    fs.readdir(resolvedPath, function(err, files) {
-                        if (err) {
-                          if (logLevel === 'trace') logger.error('gebo-imagemagick, fs.readdir:', err);
-                          deferred.resolve({ error: err });
-                        }
-                        // Converting a PDF may potentially generate multiple files. I.e., one for each page.
-                        if (files.length > 1) {
-                          var zipFileName = resolvedPath + '/' + outputFileName + '.zip';
-                          var output = fs.createWriteStream(zipFileName);
-                          var archive = archiver('zip');
-
-                          output.on('close', function() {
-                                if (logLevel === 'trace') logger.info('gebo-imagemagick, archiver:', archive.pointer() + ' total bytes in ' + zipFileName);
-                                deferred.resolve(zipFileName);
-                            });
-
-                          archive.on('error', function(err) {
-                                if (logLevel === 'trace') logger.error('gebo-imagemagick, archiver:', err);
-                                deferred.resolve({ error: err });
-                            });
-
-                          archive.pipe(output);
-
-                          files.forEach(function(file) {
-                                archive.append(fs.createReadStream(resolvedPath + '/' + file), { name: file });
-                            });
-                          archive.finalize(); }
-                        else {
-                          deferred.resolve(resolvedPath + '/' +  files[0]);
-                        }
-                    });
-                });
-            }
-          });
-      });
-
-    return deferred.promise;
+    return exports;
   };
-exports.convert = _convert;
-
-
-/**
- * Take the incoming filename and its extension
- * and return the hypothetical output filename
- *
- * @param string
- * @param string
- *
- * @return string
- */
-function _getOutputFileName(path, extension) {
-    var filename = path.split('/');
-    filename = filename[filename.length - 1];
-    filename = filename.split('.');
-
-    // No extension found
-    if (filename.length === 1) {
-      return filename[0] + '.' + extension;
-    }
-
-    // Hidden file
-    if (filename[0] === '') {
-      filename = filename.slice(1);
-      filename[0] = '.' + filename[0];
-      if (filename.length === 1) {
-        return filename[0] + '.' + extension;
-      }
-    }
-
-    filename = filename.slice(0, -1);
-    
-    return filename + '.' + extension;
-  };
-exports.getOutputFileName = _getOutputFileName;
-
